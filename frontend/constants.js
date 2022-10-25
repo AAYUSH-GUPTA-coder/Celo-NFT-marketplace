99e054ce7065e9e41d8201bc607d10c6e00407eb
AAYUSH-GUPTA-coder <60979345+AAYUSH-GUPTA-coder@users.noreply.github.com>
2022-10-25 12:52:58 +0530

    Upgraded Frontend

---
+++ frontend/constants.js
@@ -0,0 +1,5 @@
+export const SUBGRAPH_URL =
+    "https://api.thegraph.com/subgraphs/name/aayush-gupta-coder/celo-nft-marketplace";
+  
+  
+export const MARKETPLACE_ADDRESS = 0xdb3071925173fee9c72cb274a38c0e60f0246b75;
